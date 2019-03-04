1f7b48b40ca113e53e56bc7923f829dc6cdcbbba
Christoph Neuroth <christoph.neuroth@gmail.com>
2019-03-04 15:31:57 +0100

    Haskell Programming Chapter 12 Unfold Exercise

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 import Test.Hspec
 import BinaryTree
+import Data.List (unfoldr)
 
 main :: IO ()
 main = hspec $ do
@@ -33,3 +34,41 @@ main = hspec $ do
 
         it "works" $ do
           foldTree (+) 0 tree `shouldBe` 6
+
+  describe "Chapter 12 / unfolds" $ do
+    describe "iterate" $ do
+      let
+        myIterate :: (a -> a) -> a -> [a]
+        myIterate f a = a : myIterate f (f a)
+
+      it "builds infinite list of things" $ do
+        take 5 (myIterate id (1::Integer)) `shouldBe` ([1,1,1,1,1] :: [Integer])
+        take 5 (myIterate id (5::Integer)) `shouldBe` ([5,5,5,5,5] :: [Integer])
+        take 5 (myIterate (+ 1) (0::Integer)) `shouldBe` ([0,1,2,3,4] :: [Integer])
+
+    describe "unfoldr" $ do
+      let
+        myUnfoldr :: (b -> Maybe (a,b)) -> b -> [a]
+        myUnfoldr f b = case f b of
+          Just (a,b2) -> a : myUnfoldr f b2
+          Nothing -> []
+        add1 = (\b -> Just (b, b+1))
+        result = myUnfoldr add1 (1::Integer)
+
+      it "can be used like iterate, but more general" $ do
+       take 5 result `shouldBe` ([1,2,3,4,5] :: [Integer])
+
+      it "works exactly like Data.List.unfoldr" $ do
+        take 1000 result `shouldBe` (take 1000 $ unfoldr add1 (1::Integer))
+
+      it "ends the list when function returns Nothing" $ do
+        let
+          add1Until10 = (\b -> if b <= 10 then Just (b, b+1) else Nothing)
+        myUnfoldr add1Until10 (1::Integer) `shouldBe` [1,2,3,4,5,6,7,8,9,10]
+
+    describe  "myBetterIterate" $ do
+      it "is defined in terms of myUnfoldr" $ do
+        let
+          myBetterIterate :: (a -> a) -> a -> [a]
+          myBetterIterate f = unfoldr (\a -> Just (a, f a))
+        take 5 (myBetterIterate (+ 1) (0::Integer)) `shouldBe` ([0,1,2,3,4] :: [Integer])
